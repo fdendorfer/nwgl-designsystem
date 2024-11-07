f18e5c7c703c515a185d6f406fa73ce438bf0339
fdendorfer <fdendorfer@users.noreply.github.com>
2024-11-07 14:32:46 +0000

    Deploying to gh-pages from @ fdendorfer/nwgl-designsystem@b5eebf8ad39c212d7f648288c4e708d5c175a4cd 🚀

--- assets/TopBar-DS0GOHfr.js
+++
@@ -1,7 +0,0 @@
-import{ae as t,af as s,ag as i,ah as p}from"./index-TV7OS9KR.js";import{u as a}from"./index-D-71JEuq.js";import"./TopBar-BP52QXMm.js";import{T as m,E as n}from"./TopBar.stories--wOxUsX8.js";import"./iframe-Cf5ITgu2.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-CxISiSMm.js";import"./index-DrFu-skq.js";import"./vue.esm-bundler-8gVuwTo0.js";import"./Badge-mN4njV8j.js";import"./SvgIcon-DBT14I0p.js";import"./Btn-Cr3Tjce-.js";import"./LanguageSwitcher-tDfIDR9t.js";import"./Select-rmYmztcG.js";import"./TopBarNavigation-BA3mB4p9.js";function e(o){const r={h1:"h1",hr:"hr",...a(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:m}),`
-`,t.jsx(r.h1,{id:"top-bar",children:"Top Bar"}),`
-`,t.jsx(i,{of:n}),`
-`,t.jsx(p,{of:n}),`
-`,t.jsx("a",{href:"?path=/story/sections-header-topbar--example",children:"Go to the Canvas Tab"}),`
-`,t.jsx(r.hr,{}),`
-`,t.jsx("a",{href:"?id=sections-header-topbar--example",target:"_blank",children:"Open page in full width in a new tab"})]})}function _(o={}){const{wrapper:r}={...a(),...o.components};return r?t.jsx(r,{...o,children:t.jsx(e,{...o})}):e(o)}export{_ as default};
