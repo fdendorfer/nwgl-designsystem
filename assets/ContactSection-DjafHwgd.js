4877c5618700ae8639a47b78e420bf4c4abaebe2
fdendorfer <fdendorfer@users.noreply.github.com>
2024-11-21 14:36:09 +0000

    Deploying to gh-pages from @ fdendorfer/nwgl-designsystem@b5eebf8ad39c212d7f648288c4e708d5c175a4cd 🚀

--- assets/ContactSection-DjafHwgd.js
+++
@@ -1,7 +0,0 @@
-import{ae as t,af as i,ag as s,ah as m}from"./index-C-MLwi7v.js";import{u as a}from"./index-DkssTCgU.js";import"./ContactSection-CGLorok_.js";import{C as c,E as e}from"./ContactSection.stories-C8vDYvMS.js";import"./iframe-CY4jXn0A.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-pVbLjA8_.js";import"./index-DrFu-skq.js";import"./vue.esm-bundler-8gVuwTo0.js";import"./Btn-D0742sYv.js";import"./SvgIcon-CEUTN6H6.js";import"./Contact-D3GHNYEG.js";import"./AccordionItem-L0AFLu1F.js";import"./Accordion-3JELBM4F.js";function r(o){const n={h1:"h1",hr:"hr",...a(),...o.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:c}),`
-`,t.jsx(n.h1,{id:"contact-section",children:"Contact Section"}),`
-`,t.jsx(s,{of:e}),`
-`,t.jsx(m,{of:e}),`
-`,t.jsx("a",{href:"?path=/story/sections-contact--example",children:"Go to the Canvas Tab"}),`
-`,t.jsx(n.hr,{}),`
-`,t.jsx("a",{href:"?id=sections-content-contact--example&viewMode=story",target:"_blank",children:"Open page in full width in a new tab"})]})}function E(o={}){const{wrapper:n}={...a(),...o.components};return n?t.jsx(n,{...o,children:t.jsx(r,{...o})}):r(o)}export{E as default};
