60ab69c035115e5051b9c6b4b4926166bd87847a
fdendorfer <flo.dendorfer@gmail.com>
2025-01-06 14:53:12 +0000

    Deploying to gh-pages from @ fdendorfer/nwgl-designsystem@b7504db513429f53be9303ff536416fadcfec42d 🚀

---
+++ assets/Glossary-BnSn_Ng7.js
@@ -0,0 +1,18 @@
+import{ae as s,af as a,ag as e}from"./index-D13lCLwa.js";import{u as o}from"./index-BR5dc24o.js";import{G as n,W as l,a as h,b as p}from"./Glossary.stories-DEYii7xa.js";import"./iframe-vGZFE3UK.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-pVbLjA8_.js";import"./index-DrFu-skq.js";import"./vue.esm-bundler-8gVuwTo0.js";import"./CarouselBadgeFilter-Vuo4wcIF.js";import"./Badge-B5E7aSsw.js";import"./SvgIcon-YW-tbzRN.js";import"./swiper-vue-CK8r6Ueo.js";import"./Btn-C3wUxN7I.js";import"./Accordion-3JELBM4F.js";import"./Select-rmYmztcG.js";import"./AlterBodyClasses-BShIwxIy.js";import"./TopHeader-C_QvcQR8.js";import"./pinia-C9w1FlR-.js";import"./LanguageSwitcher-tDfIDR9t.js";import"./Logo-adldwgE6.js";import"./Input-OHQ8Yd2z.js";import"./DesktopMenu-rzgRRAlp.js";import"./MainNavigation-AGXlckA-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./FooterInformation-IuI2eLfh.js";import"./FooterNavigation-BMxCvS8v.js";import"./MobileMenu-CGzq4AjJ.js";import"./TopBarNavigation-CRMryryw.js";import"./v4-Ch9TGLTe.js";import"./TopBar-CAmLLHWq.js";function i(r){const t={h1:"h1",hr:"hr",p:"p",...o(),...r.components};return s.jsxs(s.Fragment,{children:[s.jsx(a,{of:n}),`
+`,s.jsx(t.h1,{id:"glossar-results-with-sticky-search",children:"Glossar results with sticky search"}),`
+`,s.jsx(t.p,{children:"Please use full width view in order to see the sticky search working correctly."}),`
+`,s.jsx("a",{href:"?path=/story/pages-glossar--with-results-sticky",children:"Go to the Canvas Tab"}),`
+`,s.jsx(t.hr,{}),`
+`,s.jsx("a",{href:"?id=pages-glossar--with-results-sticky&args=&viewMode=story",target:"_blank",children:"Open page in full width in a new tab"}),`
+`,s.jsx(e,{of:l}),`
+`,s.jsx(t.h1,{id:"glossar-results",children:"Glossar results"}),`
+`,s.jsx("a",{href:"?path=/story/pages-glossar--with-results",children:"Go to the Canvas Tab"}),`
+`,s.jsx(t.hr,{}),`
+`,s.jsx("a",{href:"?id=pages-glossar--with-results&args=&viewMode=story",target:"_blank",children:"Open page in full width in a new tab"}),`
+`,s.jsx(e,{of:h}),`
+`,s.jsx(t.h1,{id:"glossar-results-with-sticky-search-and-filters",children:"Glossar results with sticky search and filters"}),`
+`,s.jsx(t.p,{children:"Please use full width view in order to see the sticky search working correctly."}),`
+`,s.jsx("a",{href:"?path=/story/pages-glossar--with-results-sticky-and-filters",children:s.jsx(t.p,{children:"Go to the Canvas Tab"})}),`
+`,s.jsx(t.hr,{}),`
+`,s.jsx("a",{href:"?id=pages-glossar--with-results-sticky-and-filters&args=&viewMode=story",target:"_blank",children:s.jsx(t.p,{children:"Open page in full width in a new tab"})}),`
+`,s.jsx(e,{of:p})]})}function H(r={}){const{wrapper:t}={...o(),...r.components};return t?s.jsx(t,{...r,children:s.jsx(i,{...r})}):i(r)}export{H as default};
---
+++ assets/SearchResultsFilters-DoexEOiP.js
@@ -0,0 +1,16 @@
+import{ae as t,af as o,ag as s}from"./index-D13lCLwa.js";import{u as a}from"./index-BR5dc24o.js";import{S as n,W as h,a as p,b as l}from"./SearchResultsFilters.stories-BfruenmG.js";import"./iframe-vGZFE3UK.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-pVbLjA8_.js";import"./index-DrFu-skq.js";import"./vue.esm-bundler-8gVuwTo0.js";import"./AlterBodyClasses-BShIwxIy.js";import"./TopHeader-C_QvcQR8.js";import"./pinia-C9w1FlR-.js";import"./LanguageSwitcher-tDfIDR9t.js";import"./Select-rmYmztcG.js";import"./Logo-adldwgE6.js";import"./SvgIcon-YW-tbzRN.js";import"./Input-OHQ8Yd2z.js";import"./TopBar-CAmLLHWq.js";import"./Badge-B5E7aSsw.js";import"./Btn-C3wUxN7I.js";import"./TopBarNavigation-CRMryryw.js";import"./DesktopMenu-rzgRRAlp.js";import"./MainNavigation-AGXlckA-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./MobileMenu-CGzq4AjJ.js";import"./swiper-vue-CK8r6Ueo.js";import"./v4-Ch9TGLTe.js";import"./FooterInformation-IuI2eLfh.js";import"./FooterNavigation-BMxCvS8v.js";import"./SearchResultsList-CoHVytiw.js";import"./MetaInfo-3mcCqzZ_.js";import"./Card-C-sGDILy.js";import"./Pagination-D83rHNOn.js";import"./Notification-D13FFznZ.js";function i(e){const r={h1:"h1",hr:"hr",...a(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(o,{of:n}),`
+`,t.jsx(r.h1,{id:"search-results-with-filters",children:"Search results, with filters"}),`
+`,t.jsx("a",{href:"?path=/story/pages-search-results-with-filters--with-results",children:"Go to the Canvas Tab"}),`
+`,t.jsx(r.hr,{}),`
+`,t.jsx("a",{href:"?id=pages-search-results-with-filters--with-results&args=&viewMode=story",target:"_blank",children:"Open page in full width in a new tab"}),`
+`,t.jsx(s,{of:h}),`
+`,t.jsx(r.h1,{id:"no-results",children:"No results"}),`
+`,t.jsx("a",{href:"?path=/story/pages-search-results-advanced--with-results",children:"Go to the Canvas Tab"}),`
+`,t.jsx(r.hr,{}),`
+`,t.jsx("a",{href:"?id=pages-search-results-advanced--without-results&args=&viewMode=story",target:"_blank",children:"Open page in full width in a new tab"}),`
+`,t.jsx(s,{of:p}),`
+`,t.jsx(r.h1,{id:"loading-example",children:"Loading example"}),`
+`,t.jsx("a",{href:"?path=/story/pages-search-results-advanced--when-loading",children:"Go to the Canvas Tab"}),`
+`,t.jsx(r.hr,{}),`
+`,t.jsx("a",{href:"?id=pages-search-results-advanced--when-loading&args=&viewMode=story",target:"_blank",children:"Open page in full width in a new tab"}),`
+`,t.jsx(s,{of:l})]})}function K(e={}){const{wrapper:r}={...a(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(i,{...e})}):i(e)}export{K as default};
